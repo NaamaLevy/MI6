9db34b7c0a439fe6467895aa9dcccc12af8a4fdd
Naama-Le <naamalevy25@gmail.com>
2020-01-02 16:14:27 +0200

    MessageBrokerImpl

--- src/main/java/bgu/spl/mics/application/subscribers/M.java
+++ src/main/java/bgu/spl/mics/application/subscribers/M.java
@@ -20,7 +20,6 @@ public class M extends Subscriber {
         super(name);
         instanceOfMB = MessageBrokerImpl.getInstance();
         diary = Diary.getInstance();
-
     }
 
     @Override
@@ -31,8 +30,16 @@ public class M extends Subscriber {
         //subscribe M to Tick BroadCast
         subscribeBroadcast(TickBroadcast.class, (TickBroadcast tick) -> time = tick.getTick());
         //subscribe M to MissionReceivedEvent
+
         Callback<MissionReceivedEvent> missionReceivedEventCallback = (MissionReceivedEvent meE) ->{
             diary.incrementTotal();
+            Report report = new Report();
+            //create a report for the mission
+            report.setTimeCreated(time);
+            report.setMissionName(meE.getMissionName());
+            report.setAgentsNames(meE.getAgentsNumbers());
+            report.setGadgetName(meE.getGadget());
+
             AgentsAvailableEvent agentsAvailableEvent = new AgentsAvailableEvent(meE.getAgentsNumbers());
             Future<Integer> isAgentsAvailableFuture = getSimplePublisher().sendEvent(agentsAvailableEvent);
             int agentsAvailable = 0;
@@ -48,65 +55,24 @@ public class M extends Subscriber {
             }
             if ((agentsAvailable==1) && (gadgetAvailable > 0) && (agentsAvailable <= meE.getExpiredTime())){
                 AgentsSendToMissionEvent agentsSendToMissionEvent = new AgentsSendToMissionEvent(meE.getAgentsNumbers(),meE.getDuration() );
-                Future<Integer> isAgentsAvailableFuture = getSimplePublisher().sendEvent(agentsAvailableEvent);
+                Future SendAgentsFuture = getSimplePublisher().sendEvent(agentsAvailableEvent);
+
+                int mName = (int) Integer.parseInt(this.getName());
+                report.setM(mName);
+                int mpName = (int)Integer.parseInt(agentsAvailableEvent.getMonneypenny());
+                report.setMoneypenny(mpName);
+                report.setTimeIssued(meE.getTimeIssued());
+                report.setQTime(meE.getTime());
+                report.setAgentsSerialNumbers(agentsAvailableEvent.getAgentsName());
+                diary.addReport(report); //adds the report to the diary
             }
-
-
+            else{
+                ReleaseAgentsEvent releaseAgentsEvent = new ReleaseAgentsEvent(meE.getAgentsNumbers());
+                Future releaseAgents = getSimplePublisher().sendEvent(releaseAgentsEvent);
+            }
         };
 
-        subscribeEvent(MissionReceivedEvent.class, (MissionReceivedEvent missionReceivedEvent) -> {
-            Diary.getInstance().incrementTotal(); // performs total++
-            int processTick = time;
+        subscribeEvent(MissionReceivedEvent.class, missionReceivedEventCallback);
 
-            // checks if agents are available (isAgentsAvailableFuture will hold the answer)
-            AgentsAvailableEvent agentsAvailableEvent = new AgentsAvailableEvent(missionReceivedEvent.getAgentsNumbers(), missionReceivedEvent.getDuration());
-            System.out.println("M: Event accepted" + " time:" + time );
-            Future<Integer> isAgentsAvailableFuture = getSimplePublisher().sendEvent(agentsAvailableEvent); //future takes the returned value of agentsAvailableEvent
-            System.out.println("M: future" + " time:" + time );
-            if (isAgentsAvailableFuture.isResolved()) {
-                int isAgentAvailable = isAgentsAvailableFuture.get();
-                if (isAgentAvailable>0) {
-                    System.out.println("M: We got the agents" + " time:" + time );
-                    GadgetAvailableEvent gadgetAvailableEvent = (new GadgetAvailableEvent<Boolean>(missionReceivedEvent.getGadget()));
-                    Future<Integer> isGadgetAvailableFuture = getSimplePublisher().sendEvent(gadgetAvailableEvent); // future takes the returned value of gadgetAvailableEvent
-                    if (isGadgetAvailableFuture != null) {
-                        int isGadgetAvailable = isGadgetAvailableFuture.get(); //takes the returned value of the future (in int, -1 represents false, true otherwise
-                        if (isGadgetAvailable > 0) { //default value is -1, if gadget is available time is set to current time (so it'll be positive)
-                            System.out.println("M: We got the gadget " + " time:" + time );
-                            if (missionReceivedEvent.getExpiredTime() > processTick && time != -1) {
-                                agentsAvailableEvent.setShouldSendAgents(true); // set shouldSendAgents to true, so Monneypenny will send the agent(s)
-                                //builds the report
-                                missionReceivedEvent.setM(this.getName());
-                                missionReceivedEvent.setMoneypenny(agentsAvailableEvent.getMonneypenny());
-                                Report report = new Report();
-                                report.setTimeCreated(time);
-                                report.setMissionName(missionReceivedEvent.getMissionName());
-                                report.setAgentsNames(missionReceivedEvent.getAgentsNumbers());
-                                report.setAgentsSerialNumbers(agentsAvailableEvent.getAgentsName());
-                                report.setGadgetName(missionReceivedEvent.getGadget());
-                                System.out.println(this.getName());
-                                System.out.println(this.getName());
-                                System.out.println(this.getName());
-                                int mName = (int) Integer.parseInt(this.getName());
-                                report.setM(mName);
-                                int mpName = (int)Integer.parseInt(agentsAvailableEvent.getMonneypenny());
-                                report.setMoneypenny(mpName);
-                                report.setTimeIssued(missionReceivedEvent.getTimeIssued());
-                                report.setQTime(gadgetAvailableEvent.getTime());
-                                Diary.getInstance().addReport(report); //adds the report to the diary
-                            }
-                        }
-                    }
-                }
-            }
-            agentsAvailableEvent.setShouldSendAgents(false); // set shouldSendAgents to true, so Monneypenny will release the agent(s)
-
-
-
-
-            }
-        );
     }
-
-
 }
